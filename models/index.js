fc78dc6145e8c68e3344fe41b448170f3141892c
Andrew <andrew@Andrews-MacBook-Pro.local>
2017-01-07 11:55:34 -0500

    resolve bugs, prepare for transactionterms

--- models/index.js
+++ models/index.js
@@ -1,32 +1,30 @@
-/*jslint node:true */
-'use strict';
+/* jslint node:true */
+"use strict";
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
-
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+var fs        = require("fs");
+var path      = require("path");
+var Sequelize = require("sequelize");
+var env       = process.env.NODE_ENV || "development";
+var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+if (process.env.DATABASE_URL) {
+  var sequelize = new Sequelize(process.env.DATABASE_URL,config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+var db        = {};
 
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (file.indexOf(".") !== 0) && (file !== "index.js");
   })
   .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
+    var model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
 Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
+  if ("associate" in db[modelName]) {
     db[modelName].associate(db);
   }
 });
